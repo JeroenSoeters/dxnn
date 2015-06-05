cc0b8c0e204d3d3fe2503fddfe69a0861a02d265
Jeroen Soeters <wfhsoeters@gmail.com>
2015-06-04 21:59:05 -0400

    Added generation field to sensor and actuator records

--- apps/dxnn/src/records.hrl
+++ apps/dxnn/src/records.hrl
@@ -1,5 +1,5 @@
--record(sensor, {id, cortex_id, name, scape, vl, fanout_ids=[]}).
--record(actuator, {id, cortex_id, name, scape, vl, fanin_ids=[]}).
+-record(sensor, {id, cortex_id, name, scape, vl, fanout_ids=[], generation}).
+-record(actuator, {id, cortex_id, name, scape, vl, fanin_ids=[], generation}).
 -record(neuron, {id, generation, cortex_id, af, input_ids_plus_weights=[], output_ids=[], recursive_output_ids=[]}).
 -record(cortex, {id, agent_id, neuron_ids=[], sensor_ids=[], actuator_ids=[]}).
 -record(agent, {id, generation, population_id, species_id, cortex_id, fingerprint, constraint, evo_hist=[], fitness, innovation_factor=0, pattern=[]}).
